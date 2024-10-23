839dc61794d3644fec4449e95ade2e4190d371e4
prathamesh707 <prathameshsurve967@gmail.com>
2024-10-23 20:43:43 +0530

    first commit

---
+++ components/Navbar.tsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import Link from 'next/link'
+import Image from 'next/image'
+import MobileNav from './MobileNav'
+import { SignedIn, UserButton } from '@clerk/nextjs'
+const Navbar = () => {
+  return (
+   <nav className='flex flex-between fixed z-50 w-full bg-dark-1 px-6 py-4 lg:px-10'>
+      <Link href="/" className='flex items-center gap-1'>
+        <Image 
+          src='/icons/logo.svg'
+          width={33}
+          height={33}
+          alt='Yoom logo'
+          className='max-sm:size-10'
+        /> 
+        <p className='text-[26px] font-extrabold text-white max-sm:hidden'>MeetWave</p>
+      </Link>
+      <div className='flex-between gap-5'>
+        <SignedIn>
+          <UserButton />
+        </SignedIn>
+
+         <MobileNav />
+      </div>
+   </nav>
+  )
+}
+
+export default Navbar
